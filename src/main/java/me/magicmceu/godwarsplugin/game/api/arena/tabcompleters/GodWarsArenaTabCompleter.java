fb3e25e85a904cfeb18348c5a8b9dd63ca210218
pixel <pixel@eloteam.tk>
2023-07-12 17:33:23 +0200

    Make this thing java 8 compatible.

--- src/main/java/me/magicmceu/godwarsplugin/game/api/arena/tabcompleters/GodWarsArenaTabCompleter.java
+++ src/main/java/me/magicmceu/godwarsplugin/game/api/arena/tabcompleters/GodWarsArenaTabCompleter.java
@@ -25,7 +25,7 @@ public class GodWarsArenaTabCompleter implements TabCompleter {
             GWarenaType = arenaType;
             return GWarenaType;
         } else {
-            return List.of("");
+            return new ArrayList<>();
         }
     }
 
